534cb003582465ebb44f5f394c94966c93f31822
JONGHYUNVAN <whdgus808@naver.com>
2025-02-24 20:12:21 +0900

    pageprops 수정정

--- src/app/post/view/[id]/page.tsx
+++ src/app/post/view/[id]/page.tsx
@@ -1,14 +1,20 @@
-import { ViewPostHeader } from '../ViewPostHeader'
-import { ViewPostContent } from '../ViewPostContent'
-import { postAPI } from '@/api/post/postApi'
+import { ViewPostHeader } from '../ViewPostHeader';
+import { ViewPostContent } from '../ViewPostContent';
+import { postAPI } from '@/api/post/postApi';
 
-export default async function ViewPostPage({ params: { id } }: { params: { id: string } }) {
-  const post = await postAPI.getPost(Number(id))
+// PageProps 타입 정의
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function ViewPostPage({ params }: PageProps) {
+  const { id } = await params;
+  const post = await postAPI.getPost(Number(id));
 
   return (
     <div className="max-w-screen-lg mx-auto">
       <ViewPostHeader />
       <ViewPostContent post={post} />
     </div>
-  )
+  );
 }

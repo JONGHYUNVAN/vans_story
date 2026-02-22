5e2bb932406b22ee14795b9a4c4b3cc06f087226
JONGHYUNVAN <whdgus808@naver.com>
2026-02-22 17:43:43 +0900

    Update dependencies and improve Vitest configuration: Upgraded various packages in package.json and package-lock.json, including AWS SDK, React, and Storybook components, to their latest versions for enhanced performance and security. Refined Vitest configuration by simplifying the test setup for Storybook integration, ensuring a more streamlined testing process.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vitest/config';
-
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
 const dirname =
@@ -10,26 +9,14 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
+  plugins: [
+    // The plugin will run tests for the stories defined in your Storybook config
+    // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+    storybookTest({ configDir: path.join(dirname, '.storybook') }),
+  ],
   test: {
-    workspace: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-        enabled: true,
-        headless: true,
-        name: 'chromium',
-        provider: 'playwright'
-      },
-          setupFiles: ['.storybook/vitest.setup.ts'],
-        },
-      },
-    ],
+    name: 'storybook',
+    environment: 'jsdom',
+    setupFiles: ['.storybook/vitest.setup.ts'],
   },
 });
